fb92af39cb754fde7110d6cdce8286c3197e9b26
Susan Meyers <smmeyers24@gmail.com>
2018-08-24 01:22:15 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,7 +17,6 @@ def fourth_challenge
       :heroine_friends => [
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
-      ]#your hashes here!
       ]
    }
 }
